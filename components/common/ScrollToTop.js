fe6b40d902ea11ba5b5b50b97479bb1aad8a5122
sksabbirhossain <sksabbirhossain4@gmail.com>
2025-08-28 16:17:16 +0600

    update scroll to top button design

--- components/common/ScrollToTop.js
+++ components/common/ScrollToTop.js
@@ -28,30 +28,36 @@ export default function ScrollToTop() {
   return (
     <>
       {isVisible && (
-        <button
-          onClick={scrollToTop}
-          className="fixed right-1/12 bottom-6 z-50 cursor-pointer rounded-full bg-emerald-600 p-3 text-white shadow-lg transition-all duration-300 hover:bg-emerald-700"
-        >
-          <motion.div
-            animate={{ y: [0, -5, 0] }} // bounce up & down
-            transition={{ repeat: Infinity, duration: 1.5, ease: "easeIn" }}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-6"
+        <div className="fixed right-1/12 bottom-6 z-50 flex h-12 w-12 items-center justify-center">
+          <div className="relative h-full w-full">
+            {/* background */}
+            <div className="absolute -top-5 -left-6 -z-10 h-24 w-24 max-w-[500rem] rounded-full bg-gradient-to-r from-indigo-500 from-10% via-red-400 via-30% to-emerald-500 to-90% blur-[45px]" />
+            <div
+              onClick={scrollToTop}
+              className="z-20 h-full w-full cursor-pointer rounded-full bg-emerald-600/80 p-3 text-white shadow-lg backdrop-blur-2xl transition-all duration-300 hover:bg-emerald-700/80"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M8.25 6.75 12 3m0 0 3.75 3.75M12 3v18"
-              />
-            </svg>
-          </motion.div>
-        </button>
+              <motion.p
+                animate={{ y: [0, -5, 0] }} // bounce up & down
+                transition={{ repeat: Infinity, duration: 1.5, ease: "easeIn" }}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M8.25 6.75 12 3m0 0 3.75 3.75M12 3v18"
+                  />
+                </svg>
+              </motion.p>
+            </div>
+          </div>
+        </div>
       )}
     </>
   );

8cd9551fa87cdb71e27b1ff686eaf23d2570d3b6
Sk Sabbir Hossain <sksabbirhossain4@gmail.com>
2025-10-09 23:04:00 +0600

    updated next-sitemap config file

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,15 +1,18 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: "https://sk-sabbir-hossain-portfolio.vercel.app",
-  generateRobotsTxt: true, // (optional)
-  sitemapSize: 7000,
+  generateRobotsTxt: true, // generate robots.txt
+  sitemapSize: 5000, // max URLs per sitemap
   changefreq: "weekly",
   priority: 0.7,
-  exclude: ["/server-sitemap.xml"], // optional if needed
+  exclude: ["/server-sitemap.xml"], // exclude any dynamic/external pages if needed
   robotsTxtOptions: {
-    policies: [{ userAgent: "*", allow: "/" }],
-    additionalSitemaps: [
-      "https://sk-sabbir-hossain-portfolio.vercel.app/sitemap.xml",
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/", // allow all
+      },
     ],
+    additionalSitemaps: [], // only add external sitemaps if needed
   },
 };
